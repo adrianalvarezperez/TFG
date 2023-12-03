95ec24458f6b767c33499b0bbb90f42a8707993f
Adrián <152741916+adrianalvarezperez@users.noreply.github.com>
2023-12-03 20:05:06 +0100

    Add files via upload

---
+++ TFG/ENTREGA FINAL/TFG Spring boot/Grupo14Bodegasyviñedos/src/test/java/Tienda/Grupo14BodegasyvinedosApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo14BodegasyvinedosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
